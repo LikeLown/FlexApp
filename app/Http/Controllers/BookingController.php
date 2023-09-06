69a4338e4e364ff3a4fb191319fc35c1a108dff2
Lown <stevencourtadon@gmail.com>
2023-09-06 15:36:56 +0200

    new file:   app/Http/Controllers/BookingController.php 	modified:   app/Providers/Filament/AdminPanelProvider.php 	new file:   app/View/Components/AppLayout.php 	modified:   package-lock.json 	modified:   package.json 	new file:   postcss.config.js 	modified:   public/favicon.ico 	modified:   resources/css/app.css 	modified:   resources/js/app.js 	deleted:    resources/js/bootstrap.js 	deleted:    resources/views/welcome.blade.php 	modified:   routes/web.php 	new file:   tailwind.config.js 	modified:   vite.config.js

---
+++ app/Http/Controllers/BookingController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Booking;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Illuminate\Http\Request;
+
+class BookingController extends Controller
+{
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {   
+        $bookings = Booking::all();
+        return view('bookings.index',['bookings' => $bookings]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+         return view('bookings.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        return view('bookings.edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}

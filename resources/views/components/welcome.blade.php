9e5840b4fbe12556afb67b19d29f68b6d6c44022
Lown <stevencourtadon@gmail.com>
2023-09-07 08:37:54 +0200

    modified:   resources/views/components/action-message.blade.php 	modified:   resources/views/components/action-section.blade.php 	modified:   resources/views/components/authentication-card.blade.php 	modified:   resources/views/components/button.blade.php 	modified:   resources/views/components/checkbox.blade.php 	modified:   resources/views/components/danger-button.blade.php 	modified:   resources/views/components/input.blade.php 	modified:   resources/views/components/secondary-button.blade.php 	modified:   resources/views/components/section-border.blade.php 	modified:   resources/views/components/section-title.blade.php 	modified:   resources/views/components/switchable-team.blade.php 	modified:   resources/views/components/validation-errors.blade.php 	modified:   resources/views/components/welcome.blade.php 	deleted:    resources/views/home.blade.php 	modified:   routes/web.php

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -1,96 +1,82 @@
 <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
-    <x-application-logo class="block h-12 w-auto" />
 
-    <h1 class="mt-8 text-2xl font-medium text-gray-900">
-        Welcome to your Jetstream application!
-    </h1>
-
-    <p class="mt-6 text-gray-500 leading-relaxed">
-        Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
-        to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
-        you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
-        ecosystem to be a breath of fresh air. We hope you love it.
-    </p>
-</div>
-
-<div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 p-6 lg:p-8">
-    <div>
-        <div class="flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6 stroke-gray-400">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
-            </svg>
-            <h2 class="ml-3 text-xl font-semibold text-gray-900">
-                <a href="https://laravel.com/docs">Documentation</a>
-            </h2>
-        </div>
-
-        <p class="mt-4 text-gray-500 text-sm leading-relaxed">
-            Laravel has wonderful documentation covering every aspect of the framework. Whether you're new to the framework or have previous experience, we recommend reading all of the documentation from beginning to end.
-        </p>
-
-        <p class="mt-4 text-sm">
-            <a href="https://laravel.com/docs" class="inline-flex items-center font-semibold text-indigo-700">
-                Explore the documentation
-
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="ml-1 w-5 h-5 fill-indigo-500">
-                    <path fill-rule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clip-rule="evenodd" />
-                </svg>
-            </a>
-        </p>
-    </div>
 
     <div>
-        <div class="flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6 stroke-gray-400">
-                <path stroke-linecap="round" d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />
-            </svg>
-            <h2 class="ml-3 text-xl font-semibold text-gray-900">
-                <a href="https://laracasts.com">Laracasts</a>
-            </h2>
+        <div role="contentinfo" class="flex items-center flex-col px-4">
+            <p tabindex="0" class="focus:outline-none uppercase text-sm text-center text-base-content leading-4">
+                En quelques minutes vous pouvez ....</p>
+            <hh1 tabindex="0"
+                class="focus:outline-none text-4xl lg:text-4xl font-extrabold text-center leading-10 text-accent-content lg:w-5/12 md:w-9/12 pt-4">
+                FlexApp</hh1>
         </div>
-
-        <p class="mt-4 text-gray-500 text-sm leading-relaxed">
-            Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
-        </p>
-
-        <p class="mt-4 text-sm">
-            <a href="https://laracasts.com" class="inline-flex items-center font-semibold text-indigo-700">
-                Start watching Laracasts
-
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="ml-1 w-5 h-5 fill-indigo-500">
-                    <path fill-rule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clip-rule="evenodd" />
-                </svg>
-            </a>
-        </p>
-    </div>
-
-    <div>
-        <div class="flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6 stroke-gray-400">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-            </svg>
-            <h2 class="ml-3 text-xl font-semibold text-gray-900">
-                <a href="https://tailwindcss.com/">Tailwind</a>
-            </h2>
+        <div tabindex="0" aria-label="group of cards"
+            class="focus:outline-none mt-20 flex flex-wrap justify-center gap-10 px-4">
+            <div tabindex="0" aria-label="card 1" class="focus:outline-none flex sm:w-full md:w-5/12 pb-20">
+                <div class="w-20 h-20 relative mr-5">
+                    <div class="absolute top-0 right-0 bg-[#70BD95] rounded w-16 h-16 mt-2 mr-1"></div>
+                    <div
+                        class="absolute text-white bottom-0 left-0 bg-[#002439] rounded w-16 h-16 flex items-center justify-center mt-2 mr-3">
+                        <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/icon_and_text-SVG1.svg" alt="drawer">
+                    </div>
+                </div>
+                <div class="w-10/12">
+                    <h2 tabindex="0" class="focus:outline-none text-lg font-bold leading-tight text-accent-content ">
+                        Outillage</h2>
+                    <p tabindex="0" class="focus:outline-none text-base  leading-normal pt-2">
+                        Gérez vos équipements partagés grâce à des formulaires d'emprunt.</p>
+                </div>
+            </div>
+            <div tabindex="0" aria-label="card 2" class="focus:outline-none flex sm:w-full md:w-5/12 pb-20">
+                <div class="w-20 h-20 relative mr-5">
+                    <div class="absolute top-0 right-0 bg-[#70BD95] rounded w-16 h-16 mt-2 mr-1"></div>
+                    <div
+                        class="absolute text-white bottom-0 left-0 bg-[#002439] rounded w-16 h-16 flex items-center justify-center mt-2 mr-3">
+                        <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/icon_and_text-SVG2.svg" alt="check">
+                    </div>
+                </div>
+                <div class="w-10/12">
+                    <h2 tabindex="0"
+                        class="focus:outline-none text-lg font-semibold leading-tight text-accent-content ">
+                        Suivi des nouveaux arrivants</h2>
+                    <p tabindex="0" class="focus:outline-none text-base  leading-normal pt-2">
+                        Suivez le parcours de vos nouveaux arrivants, de la publication des postes jusqu'à leur
+                        intégration dans le service.
+                    </p>
+                </div>
+            </div>
+            <div tabindex="0" aria-label="card 3" class="focus:outline-none flex sm:w-full md:w-5/12 pb-20">
+                <div class="w-20 h-20 relative mr-5">
+                    <div class="absolute top-0 right-0 bg-[#70BD95] rounded w-16 h-16 mt-2 mr-1"></div>
+                    <div
+                        class="absolute text-white bottom-0 left-0 bg-[#002439] rounded w-16 h-16 flex items-center justify-center mt-2 mr-3">
+                        <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/icon_and_text-SVG3.svg" alt="html tag">
+                    </div>
+                </div>
+                <div class="w-10/12">
+                    <h2 tabindex="0"
+                        class="focus:outline-none text-lg font-semibold leading-tight text-accent-content ">
+                        Suivi du plan d'actions</h2>
+                    <p tabindex="0" class="focus:outline-none text-base  leading-normal pt-2">
+                        Suivez vos plans d'action au sein de l'agence, planifiez des actions pour différents
+                        intervenants afin de faire évoluer votre structure.</p>
+                </div>
+            </div>
+            <div tabindex="0" aria-label="card 4" class="focus:outline-none flex sm:w-full md:w-5/12 pb-20">
+                <div class="w-20 h-20 relative mr-5">
+                    <div class="absolute top-0 right-0 bg-[#70BD95] rounded w-16 h-16 mt-2 mr-1"></div>
+                    <div
+                        class="absolute text-white bottom-0 left-0 bg-[#002439] rounded w-16 h-16 flex items-center justify-center mt-2 mr-3">
+                        <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/icon_and_text-SVG4.svg" alt="monitor">
+                    </div>
+                </div>
+                <div class="w-10/12">
+                    <h2 tabindex="0"
+                        class="focus:outline-none text-lg font-semibold leading-tight text-accent-content ">
+                        Accès multiplateforme</h2>
+                    <p tabindex="0" class="focus:outline-none text-base  leading-normal pt-2">
+                        Accédez à cette application web depuis n'importe où !</p>
+                </div>
+            </div>
         </div>
-
-        <p class="mt-4 text-gray-500 text-sm leading-relaxed">
-            Laravel Jetstream is built with Tailwind, an amazing utility first CSS framework that doesn't get in your way. You'll be amazed how easily you can build and maintain fresh, modern designs with this wonderful framework at your fingertips.
-        </p>
-    </div>
-
-    <div>
-        <div class="flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6 stroke-gray-400">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
-            </svg>
-            <h2 class="ml-3 text-xl font-semibold text-gray-900">
-                Authentication
-            </h2>
-        </div>
-
-        <p class="mt-4 text-gray-500 text-sm leading-relaxed">
-            Authentication and registration views are included with Laravel Jetstream, as well as support for user email verification and resetting forgotten passwords. So, you're free to get started with what matters most: building your application.
-        </p>
     </div>
 </div>
